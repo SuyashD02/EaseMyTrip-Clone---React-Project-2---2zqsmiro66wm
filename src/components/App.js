14b8491b25765cd14816e2021b9a2b28502af64e
SuyashD02 <suyashdattawade007@gmail.com>
2023-11-20 20:09:52 +0530

    hotelData

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import "../styles/App.css";
-
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+    <h1 >App</h1>
+    <h1 >App</h1>
+  
+    </div>
+  )
 }
 
 export default App;
